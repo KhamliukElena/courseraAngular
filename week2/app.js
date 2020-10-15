4588d589938fd052de3ff7aa8875da1b5d6caf42
Elena <2734.89@mail.ru>
2020-10-15 23:22:44 +0300

    functionality is implemented

--- week2/app.js
+++ week2/app.js
@@ -8,16 +8,31 @@
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
     function ToBuyController(ShoppingListCheckOffService) {
         var non_bought = this;
+        non_bought.toBuy = ShoppingListCheckOffService.getToBuy();
+        non_bought.remove = function (itemIndex) {
+            ShoppingListCheckOffService.remove(itemIndex);
+        }
     }
 
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
     function AlreadyBoughtController(ShoppingListCheckOffService) {
         var bought = this;
+        bought.alreadyBought = ShoppingListCheckOffService.getBought();
     }
 
     function ShoppingListCheckOffService () {
         var service = this;
         var toBuy = [ {quantity: 10, name: "cookies"}, {quantity: 5, name: "crisps"} ];
         var alreadyBought = [];
+        service.remove = function(itemIndex) {
+            alreadyBought.push(toBuy[itemIndex]);
+            toBuy.splice(itemIndex, 1);
+        }
+        service.getBought = function() {
+            return alreadyBought;
+        }
+        service.getToBuy = function() {
+            return toBuy;
+        }
     }
 })();
