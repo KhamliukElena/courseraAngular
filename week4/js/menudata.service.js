60436324d3d815940e386060a5b1e418f85259e3
Elena <2734.89@mail.ru>
2020-10-31 18:53:30 +0300

    point 5

---
+++ week4/js/menudata.service.js
@@ -0,0 +1,15 @@
+(function() {
+    angular.module('data').service('MenuDataService', MenuDataService);
+
+    function MenuDataService() {
+        var service = this;
+
+        service.getAllCategories = function() {
+            //return a promise which is a result of $http, all cat
+        };
+
+        service.getItemsForCategory = function() {
+            //same. but for one cat. name of the cat is passed as a part of url
+        };
+    }
+})();
