10dc2c804c98e7d67d70f18e1f37cbeee801f8f1
Elena <2734.89@mail.ru>
2020-11-22 18:47:02 +0300

    registration controller is added

--- week5/src/public/public.module.js
+++ week5/src/public/public.module.js
@@ -3,6 +3,16 @@
 /**
  * Public restaurant application. Includes the common module and ui-router.
  */
-angular.module('public', ['ui.router', 'common']);
+    angular.module('public', ['ui.router', 'common']);
 
+    angular.module('public')
+    .controller('SignUpController', SignUpController);
+        
+    function SignUpController() {
+        var reg = this;
+        
+        reg.submit = function () {
+        reg.completed = true;
+        };
+    }
 })();
